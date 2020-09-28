a6df8c417f13e0e1ee7d89654f88b35c0cd17397
Mariya <mariya.podosinova@gmail.com>
2020-09-28 20:28:24 +0100

    Managing a Component Tree

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,29 @@
 import React, { useState } from "react";
-import TodoItem from "./TodoItem"
+import TodoItem from "./TodoItem";
 
 function App() {
+  const [inputItem, setInputItem] = useState("");
+  const [items, setItems] = useState([]);
 
-    const [inputItem, setInputItem] = useState("");
-    const [items, setItems] = useState([]);
+  function handleChange(event) {
+    const newItem = event.target.value;
+    setInputItem(newItem);
+  }
 
-    function handleChange(event) {
-        const newItem= event.target.value;
-        setInputItem(newItem);     
-      }
-      
-      function addItem() {
-        setItems(prevItems => {
-          return [...prevItems, inputItem];
-        });
-        setInputItem("");
-      }
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputItem];
+    });
+    setInputItem("");
+  }
+
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
 
   return (
     <div className="container">
@@ -24,17 +31,26 @@ function App() {
         <h1>Party Guest List</h1>
       </div>
       <div className="form">
-        <input type="text" onChange={handleChange} name="item"
-          value={inputItem} placeholder="Write a Name"/>
+        <input
+          type="text"
+          onChange={handleChange}
+          name="item"
+          value={inputItem}
+          placeholder="Write a Name"
+        />
         <button>
           <span onClick={addItem}>Add</span>
         </button>
       </div>
       <div>
         <ul>
-        {items.map(item => (
+          {items.map((item, index) => (
             <TodoItem
-            text={item}/>
+              text={item}
+              onChecked={deleteItem}
+              key={index}
+              id={index}
+            />
           ))}
         </ul>
       </div>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,16 +1,15 @@
-import React, {useState} from "react";
+import React from "react";
 
 function TodoItem(props) {
-    const [isDone, setIsDone]=useState(false);
-        function handleClick(){
-    setIsDone((prevValue)=>{
-        return !prevValue;
-    })
-    }
-return <div onClick={handleClick}>
-    <li style={{textDecoration: isDone ? "line-through" : "none"}}>{props.text}</li>
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
     </div>
+  );
 }
-
 
 export default TodoItem;
